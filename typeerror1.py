335d431b548128ffa13c9ae6426f57ae9ac4d997
yoshidont-mind <tyoshida6@my.bcit.ca>
2023-10-24 19:19:35 -0700

    COMPLETE typeerror1

--- typeerror1.py
+++ typeerror1.py
@@ -3,6 +3,6 @@ I want to see whether TypeError is syntax error or not.
 """
 
 # If TypeError is syntax error, the following code is not executed.
-# If TypeError is runtime error, "Hello world" is printed before error happens.
+# If TypeError is not syntax error, "Hello world" is printed before error happens.
 print("Hello world")
 print(7 + "Hello world")
--- typeerror2.py
+++
@@ -1,4 +0,0 @@
-# when syntax error happens, the whole code is not executed
-print("This will not be printed")
-syntax_error_line = 1 2 3
-print("This won't be either")
