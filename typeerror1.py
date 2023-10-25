3a0c3c6e83f9c2b1f98c5ba06af0d9dfab25280b
yoshidont-mind <tyoshida6@my.bcit.ca>
2023-10-24 18:39:05 -0700

    ADD typeerror1, 2

---
+++ typeerror1.py
@@ -0,0 +1,8 @@
+"""
+I want to see whether TypeError is syntax error or not.
+"""
+
+# If TypeError is syntax error, the following code is not executed.
+# If TypeError is runtime error, "Hello world" is printed before error happens.
+print("Hello world")
+print(7 + "Hello world")
---
+++ typeerror2.py
@@ -0,0 +1,4 @@
+# when syntax error happens, the whole code is not executed
+print("This will not be printed")
+syntax_error_line = 1 2 3
+print("This won't be either")
